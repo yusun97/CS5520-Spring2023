130e14d5db7f796713e0b59c01575517e7b85c45
yusun97 <yusun.apply@outlook.com>
2023-01-24 17:29:01 -0800

    add confirm button

--- components/Input.js
+++ components/Input.js
@@ -1,23 +1,29 @@
-import { View, TextInput } from 'react-native'
-import { useState } from 'react';
-import React from 'react'
+import { View, TextInput, Button } from "react-native";
+import { useState } from "react";
+import React from "react";
 
-export default function Input({sendChangedText}) {
-    const [text, setText] = useState();
-
-  function changeTextHandler(changedText){
-    setText(changedText);
-    sendChangedText(changedText);
-  }
+export default function Input({ sendChangedText }) {
+  const [text, setText] = useState("default value");
 
   return (
     <View>
-      <TextInput 
-      value = {text} 
-      onChangeText = {changeTextHandler} 
-      style = {{backgroundColor:"#eee"}}
-      placeholder = "type here"
+      <TextInput
+        value={text}
+        onChangeText={(changedText) => {
+          setText(changedText);
+        }}
+        style={{ backgroundColor: "#eee" }}
+        placeholder="type here"
+      />
+
+      <Button
+        title="Confirm"
+        onPress={() => {
+          //we need arrow function, instead of just passing the function
+          sendChangedText(text);
+          setText("");
+        }}
       />
     </View>
-  )
+  );
 }
