dcded4754c8f6686c5262affdd1b03be6b7f4237
yusun97 <yusun.apply@outlook.com>
2023-01-24 18:09:23 -0800

    add cancel button

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import Header from './components/Header';
-import Input from './components/Input';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import Header from "./components/Header";
+import Input from "./components/Input";
 
 export default function App() {
   const name = "my awesome app";
   const [enteredText, setEnteredText] = useState();
-  
-  function onTextEntered(changedText){
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function onTextEntered(changedText) {
     setEnteredText(changedText);
+    setModalVisible(false);
+  }
+
+  function onCancel() {
+    setModalVisible(false);
   }
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header appName = {name}/>
-      <Input sendChangedText = {onTextEntered}/>
+      <Header appName={name} />
+      <Input
+        modelIsVisible={modalVisible}
+        onConfirm={onTextEntered}
+        cancelPress={onCancel}
+      />
+      <Button
+        title="Add a Task"
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      />
       <Text>{enteredText}</Text>
     </View>
   );
@@ -25,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- components/Input.js
+++ components/Input.js
@@ -1,29 +1,44 @@
-import { View, TextInput, Button } from "react-native";
+import { View, TextInput, Button, Modal, StyleSheet } from "react-native";
 import { useState } from "react";
 import React from "react";
 
-export default function Input({ sendChangedText }) {
+export default function Input({ onConfirm, modelIsVisible, cancelPress }) {
   const [text, setText] = useState("default value");
 
   return (
-    <View>
-      <TextInput
-        value={text}
-        onChangeText={(changedText) => {
-          setText(changedText);
-        }}
-        style={{ backgroundColor: "#eee" }}
-        placeholder="type here"
-      />
+    <Modal visible={modelIsVisible}>
+      <View style={styles.container}>
+        <TextInput
+          value={text}
+          onChangeText={(changedText) => {
+            setText(changedText);
+          }}
+          style={{ backgroundColor: "#eee" }}
+          placeholder="type here"
+        />
 
-      <Button
-        title="Confirm"
-        onPress={() => {
-          //we need arrow function, instead of just passing the function
-          sendChangedText(text);
-          setText("");
-        }}
-      />
-    </View>
+        <Button
+          title="Confirm"
+          onPress={() => {
+            //we need arrow function, instead of just passing the function
+            onConfirm(text);
+            setText("");
+          }}
+        />
+        <Button
+          title="Cancel"
+          onPress={cancelPress}
+        />
+      </View>
+    </Modal>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
